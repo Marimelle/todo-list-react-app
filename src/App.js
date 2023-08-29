ed3ac27696d27a17ee74b5f80060928a1244eb97
Marimelle <marimelle.p.dumo@gmail.com>
2023-08-30 00:05:06 +0800

    Add jsx for displaying input, button for adding task and div displaying list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [tasksList, setTasksList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleInputChange = (e) => {
+    setNewTask(e.target.value);
+  }
+  const handleClick = () => {
+    newTask !== "" ? setTasksList([...tasksList, newTask]) : console.log("clicked while input is empty");
+    setNewTask("");
+  }
+
   return (
     <div className="App">
+      <div className="addTask">
+        <input onChange={handleInputChange}/>
+        <button onClick={handleClick}>Add new task</button>
+      </div>
+      <div className="tasksList">
+        { (tasksList.length>0) ? tasksList.map(
+          (task, key) => <p>{(key+1) + ".)"} {task}</p>
+          ) : "No saved tasks."
+        }
+      </div>
     </div>
   );
 }
