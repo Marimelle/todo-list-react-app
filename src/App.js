a8fc0c865b067cd554e459ace0aceaa952794cb8
Marimelle <marimelle.p.dumo@gmail.com>
2023-08-31 01:02:00 +0800

    Add functionality for marking task as done

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
     if (newTask !== "") {
       const task = {
         taskId: list.length===0 ? 1 : list[list.length-1].taskId + 1,
+        completed: false,
         taskName: newTask
       }
       setList([...list, task]);
@@ -25,9 +26,18 @@ function App() {
       current.taskId !== taskId
     ));
   }
+  const taskCompleted = (taskId) => {
+    setList(list.map((current) => {
+      if(current.taskId === taskId){
+        current.completed = !current.completed;
+      }
+      return current;
+    }));
+  }
 
   return (
     <div className="App">
+      <h1>To-do List</h1>
       <div className="addTask">
         <input onChange={handleInputChange}/>
         <button onClick={handleClick}>Add task</button>
@@ -35,7 +45,7 @@ function App() {
       <div className="tasksList">
         { (list.length>0) ? list.map(
           (task) => {
-            return <Task taskName={task.taskName} taskId={task.taskId} deleteTask={deleteTask} />
+            return <Task taskName={task.taskName} taskId={task.taskId} completed={task.completed} deleteTask={deleteTask} taskCompleted={taskCompleted}/>
         }
           ) : "No saved tasks."
         }
--- src/Task.js
+++ src/Task.js
@@ -1,8 +1,11 @@
 export default function Task(props) {
   return (
-    <div>
-      <p>{props.taskId + ".)"} {props.taskName}</p>
-      <button onClick={()=> props.deleteTask(props.taskId)}>X</button>
+    <div className={props.completed ? "taskCompleted" : ""}>
+      <p>{props.taskId + ".) "} {props.taskName}</p>
+      <div className="btns">
+        <button className="completeBtn" onClick={()=> props.taskCompleted(props.taskId)}>{props.completed? "✔\nDONE" :"MARK\nDONE"}</button>
+        <button className="deleteBtn" onClick={()=> props.deleteTask(props.taskId)}>〤</button>
+      </div>
     </div>
   );
 }
