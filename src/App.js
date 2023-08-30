4b703e1a54fa5fdd6f5dafa6e1227b0f1f3d8ffd
Marimelle <marimelle.p.dumo@gmail.com>
2023-08-30 12:39:26 +0800

    Create functions for adding, deleting tasks

--- src/App.js
+++ src/App.js
@@ -2,15 +2,27 @@ import './App.css';
 import {useState} from 'react';
 
 function App() {
-  const [tasksList, setTasksList] = useState([]);
+  const [list, setList] = useState([]);
   const [newTask, setNewTask] = useState("");
 
   const handleInputChange = (e) => {
     setNewTask(e.target.value);
   }
   const handleClick = () => {
-    newTask !== "" ? setTasksList([...tasksList, newTask]) : console.log("clicked while input is empty");
-    setNewTask("");
+    if (newTask !== "") {
+      const task = {
+        taskId: list.length===0 ? 1 : list[list.length-1].taskId + 1,
+        taskName: newTask
+      }
+      setList([...list, task]);
+    }
+    else alert("Input is empty");
+  }
+
+  const deleteTask = (taskId) => {
+    setList(list.filter((current)=>
+      current.taskId !== taskId
+    ));
   }
 
   return (
@@ -20,8 +32,12 @@ function App() {
         <button onClick={handleClick}>Add new task</button>
       </div>
       <div className="tasksList">
-        { (tasksList.length>0) ? tasksList.map(
-          (task, key) => <p>{(key+1) + ".)"} {task}</p>
+        { (list.length>0) ? list.map(
+          (task) => {
+            return (
+            <p>{task.taskId + ".)"} {task.taskName}<button onClick={()=> deleteTask(task.taskId)}>X</button></p>
+            );
+        }
           ) : "No saved tasks."
         }
       </div>
